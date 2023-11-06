51508e5205200f65d002c1a21eb5d03cbdc3cd6a
tb-mbakalar <138683298+tb-mbakalar@users.noreply.github.com>
2023-11-06 09:29:59 -0500

    Multi class classifier for cryptic seq data.

--- datasets/one_hot.py
+++ datasets/one_hot.py
@@ -10,17 +10,18 @@ translation_dict = {'A':0,'T':1,'C':2,'G':3,'N':4}
 # One-hot encoding of sequences
 class Dataset(torch.utils.data.Dataset):
 
-  def __init__(self, sequences, labels, vocab_size):
+  def __init__(self, sequences, labels, vocab_size, output_size=1):
     self.sequences = sequences
     self.labels = labels
     self.vocab_size = vocab_size
+    self.output_size = output_size
 
   def __len__(self):
     return len(self.sequences)
 
   def __getitem__(self, idx):
     sequence = self.sequences[idx]
-    label = self.labels[idx]
+    label = F.one_hot(torch.tensor(self.labels[idx]), num_classes=self.output_size)
     encoding = torch.tensor([translation_dict[c] for c in sequence])
     x = F.one_hot(encoding, num_classes=self.vocab_size).to(torch.float32)
     return x, label
--- models/mlp.py
+++ models/mlp.py
@@ -8,39 +8,24 @@ import torch.utils.data
 A simple fully connected neural network
 '''
 class Model(nn.Module):
-  def __init__(self, input_size, hidden_size, output_size, n_hidden=0, dropout=0.5):
+  def __init__(self, input_size, hidden_size, output_size=1, n_hidden=1, dropout=0.2):
     super().__init__()
+
+    # Input layer
     self.flatten = nn.Flatten()
 
-    # Build a stack of linear layers with dropout
-    self.fc_layers = nn.ModuleList()
-    self.dropout_layers = nn.ModuleList()
-
-    if n_hidden == 1:
-      # Do not add dropout for a single-layer network
-      self.fc_layers.append(nn.Linear(input_size, output_size))
-      self.dropout_layers.append(nn.Dropout(0))
-  
-    else:
-      # Multi layer netowrk
-      self.fc_layers.append(nn.Linear(input_size, hidden_size))
-      self.dropout_layers.append(nn.Dropout(dropout))
-
-      # Add the hidden layers
-      for i in range(0, n_hidden-1):
-          self.fc_layers.append(nn.Linear(hidden_size, hidden_size))
-          self.dropout_layers.append(nn.Dropout(dropout))
-
-      # Add the output layer
-      self.fc_layers.append(nn.Linear(hidden_size, output_size))
-      self.dropout_layers.append(nn.Dropout(0))
+    # Neural network
+    self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_size, output_size),
+        )
 
   def forward(self, x):
     x = self.flatten(x)
-
-    # Fully connected multilayer network
-    for i, layer in enumerate(self.fc_layers):
-        x = F.relu(layer(x))
-        x = self.dropout_layers[i](x)
-
+    x = self.linear_relu_stack(x)
     return x
