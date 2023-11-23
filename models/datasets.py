a8dbe3e8372960cca88057ec8ae109116c9f6572
tb-mbakalar <138683298+tb-mbakalar@users.noreply.github.com>
2023-11-23 14:29:30 -0500

    Enable full prediction

--- models/datasets.py
+++ models/datasets.py
@@ -94,8 +94,7 @@ class GenomeBoxcarDataset(Dataset):
     )
     
     # Collect fasta index information
-    #self.length = len(self.fasta.seqs[self.chr]) - window_length
-    self.length = 1000000
+    self.length = len(self.fasta.seqs[self.chr]) - window_length
     self.start = 0
 
     self.return_augs = return_augs
